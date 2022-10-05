148ea76f9948d8594a533543d21d434e770e63a9
nikagelo <nikagelo@gmail.com>
2022-10-05 20:24:29 +0400

    added black version of sliders

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// First slider JS
 const nextBtn = document.querySelector("#next"),
   prevBtn = document.querySelector("#prev"),
   sliders = document.querySelectorAll(".sliderItem");
@@ -11,7 +12,6 @@ function initSlider() {
 }
 
 function renderSlides() {
-  console.log("activeIndex", activeIndex);
   sliders.forEach((slide, i) => {
     if (i === activeIndex) {
       slide.classList.add("active");
@@ -39,3 +39,129 @@ function showPrevSlide() {
   renderSlides();
 }
 initSlider();
+
+//Products JS
+const nextBtnProducts = document.querySelector("#nextProducts"),
+  prevBtnProducts = document.querySelector("#prevProducts"),
+  slidersProducts = document.querySelectorAll(".sliderItemTop");
+let activeIndexProducts = 0;
+
+function initSliderProducts() {
+  nextBtnProducts.addEventListener("click", showNextSlideProduct);
+  prevBtnProducts.addEventListener("click", showPrevSlideProduct);
+
+  renderSlidesProduct();
+}
+
+function renderSlidesProduct() {
+  slidersProducts.forEach((slide, i) => {
+    if (i === activeIndexProducts) {
+      slide.classList.add("active");
+    } else {
+      slide.classList.remove("active");
+    }
+  });
+}
+
+function showNextSlideProduct() {
+  if (activeIndexProducts === slidersProducts.length - 1) {
+    activeIndexProducts = 0;
+  } else {
+    activeIndexProducts++;
+  }
+  renderSlidesProduct();
+}
+
+function showPrevSlideProduct() {
+  if (activeIndexProducts === 0) {
+    activeIndexProducts = slidersProducts.length - 1;
+  } else {
+    activeIndexProducts--;
+  }
+  renderSlidesProduct();
+}
+initSliderProducts();
+// Best Selllers JS
+
+const nextBtnBestSeller = document.querySelector("#nextBestSeller"),
+  prevBtnBestSeller = document.querySelector("#prevBestSeller"),
+  slidersBestSeller = document.querySelectorAll(".bestSellerItem");
+let activeBestSeller = 0;
+
+function initSliderBestSeller() {
+  nextBtnBestSeller.addEventListener("click", showNextSlideBestSeller);
+  prevBtnBestSeller.addEventListener("click", showPrevSlideBestSeller);
+
+  renderSlidesBestSeller();
+}
+
+function renderSlidesBestSeller() {
+  slidersBestSeller.forEach((slide, i) => {
+    if (i === activeBestSeller) {
+      slide.classList.add("activeBestSeller");
+    } else {
+      slide.classList.remove("activeBestSeller");
+    }
+  });
+}
+
+function showNextSlideBestSeller() {
+  if (activeBestSeller === slidersBestSeller.length - 1) {
+    activeBestSeller = 0;
+  } else {
+    activeBestSeller++;
+  }
+  renderSlidesBestSeller();
+}
+
+function showPrevSlideBestSeller() {
+  if (activeBestSeller === 0) {
+    activeBestSeller = slidersBestSeller.length - 1;
+  } else {
+    activeBestSeller--;
+  }
+  renderSlidesBestSeller();
+}
+initSliderBestSeller();
+// Latest
+
+const nextBtnLatest = document.querySelector("#nextLatest"),
+  prevBtnLatest = document.querySelector("#prevLatest"),
+  slidersLatest = document.querySelectorAll(".sliderItemLatest");
+let activeLatest = 0;
+
+function initSliderLatest() {
+  nextBtnLatest.addEventListener("click", showNextSlideLatest);
+  prevBtnLatest.addEventListener("click", showPrevSlideLatest);
+
+  renderSlidesLatest();
+}
+
+function renderSlidesLatest() {
+  slidersLatest.forEach((slide, i) => {
+    if (i === activeLatest) {
+      slide.classList.add("activeLatest");
+    } else {
+      slide.classList.remove("activeLatest");
+    }
+  });
+}
+
+function showNextSlideLatest() {
+  if (activeLatest === slidersLatest.length - 1) {
+    activeLatest = 0;
+  } else {
+    activeLatest++;
+  }
+  renderSlidesLatest();
+}
+
+function showPrevSlideLatest() {
+  if (activeLatest === 0) {
+    activeLatest = slidersLatest.length - 1;
+  } else {
+    activeLatest--;
+  }
+  renderSlidesLatest();
+}
+initSliderLatest();
